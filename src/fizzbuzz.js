2702cbd9216c5755d312bb90763ba692124ce47a
Siooban <siooban@gmail.com>
2022-01-14 17:52:06 +0100

    correction

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,12 +1,12 @@
 function fizzBuzz (n) {
-    
 
-    if(n%3==0){
-        return "Fizz"
+
+    if(n%3==0 && n%5==0){
+        return "Fizzbuzz"
     }
     else if (n%5==0){return "Buzz"}
-    else if(n%3==0 && n%5==0){
-    return "Fizzbuzz"}
+    else if(n%3==0 ){return "Fizz"}
+
 
 }
 fizzBuzz(3)
