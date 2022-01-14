274a046812c5a158ae70bf49389b72c13df1ac7a
Salomé Lepers <71492814+Siooban@users.noreply.github.com>
2022-01-14 17:01:16 +0100

    code fizzBuzz.js

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,16 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    
+
+    if(n%3==0){
+        return "Fizz"
+    }
+    else if (n%5==0){return "Buzz"}
+    else if(n%3==0 && n%5==0){
+    return "Fizzbuzz"}
+
 }
+fizzBuzz(3)
+fizzBuzz(5)
+fizzBuzz(15)
 
 module.exports = fizzBuzz
