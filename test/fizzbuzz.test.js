b59bf3ccca381497396290caec1b16940c7f48e0
Siooban <siooban@gmail.com>
2022-01-14 17:52:59 +0100

    changeTest

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,16 +2,16 @@ const fizzBuzz = require("../src/fizzbuzz")
 
 test('fizzbuzz case 3', () => {
 
-    expect(fizzBuzz(33).toBe("Fizz"))
+    expect(fizzBuzz(33)).toBe("Fizz")
 })
 test('fizzbuzz case 5 ',() => {
-    expect(fizzBuzz(25).toBe("Buzz"))
+    expect(fizzBuzz(25)).toBe("Buzz")
 })
 
 test('fizzbuzz case 15 ',() => {
-    expect(fizzBuzz(30).toBe("Fizzbuzz"))
+    expect(fizzBuzz(30)).toBe("Fizzbuzz")
 })
 
 test('fizzbuzz case none ',() => {
-    expect(fizzBuzz(11).toBe())
+    expect(fizzBuzz(11)).toBe(undefined)
 })
