0c460431acab9d4112a3989df9aabde82400dc53
Salomé Lepers <71492814+Siooban@users.noreply.github.com>
2022-01-14 17:18:59 +0100

    add unit tests

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,17 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    // TODO: Tester fizzbuzz()
+test('fizzbuzz case 3', () => {
+
+    expect(fizzBuzz(33).toBe("Fizz"))
+})
+test('fizzbuzz case 5 ',() => {
+    expect(fizzBuzz(25).toBe("Buzz"))
+})
+
+test('fizzbuzz case 15 ',() => {
+    expect(fizzBuzz(30).toBe("Fizzbuzz"))
+})
+
+test('fizzbuzz case none ',() => {
+    expect(fizzBuzz(11).toBe())
 })
