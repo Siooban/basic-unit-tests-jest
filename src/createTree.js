4e91752fc45107551039add9797475fa40c85988
Salomé Lepers <71492814+Siooban@users.noreply.github.com>
2022-01-28 16:07:53 +0100

    add createTree

---
+++ src/createTree.js
@@ -0,0 +1,26 @@
+function createTree(t){
+    let current=0
+const map= new Map();
+    while (current<t.length) {
+let name=t[current]
+t[current]
+    }
+
+
+}
+class element{
+    constructor(id, name, parent){
+    this.id=id;
+    this.name=name;
+    this.parent=parent;}
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id();
+    }
+    getParent(){
+        return this.parent;
+    }
+}
+module.exports=createTree
